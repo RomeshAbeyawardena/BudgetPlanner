692712e146224c5c6c45fdaf409f70d913773fc9
Anthony Abeyawardena <Anthony.Abeyawardena@connellsgroup.co.uk>
2020-01-28 11:56:44 +0000

    Added identity and modifier fields

--- BudgetPlanner.Domains/Data/Budget.cs
+++ BudgetPlanner.Domains/Data/Budget.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using DNI.Shared.Domains.Enumerations;
+using DNI.Shared.Services.Attributes;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +11,16 @@ namespace BudgetPlanner.Domains.Data
 {
     public class Budget
     {
+        [Key]
+        public int Id { get; set; }
+
         public string Reference { get; set; }
         public bool Active { get; set; }
+        
+        [Modifier(ModifierFlag.Modified)]
         public DateTime LastUpdated { get; set; }
+        
+        [Modifier(ModifierFlag.Created)]
+        public DateTime Created { get; set; }
     }
 }
--- BudgetPlanner.Domains/Data/Transaction.cs
+++ BudgetPlanner.Domains/Data/Transaction.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,9 @@ namespace BudgetPlanner.Domains.Data
 {
     public class Transaction
     {
+        [Key]
+        public int Id { get; set; }
+
         public bool Active { get; set; }
         public int BudgetId { get; set; }
         public DateTime Created { get; set; }
