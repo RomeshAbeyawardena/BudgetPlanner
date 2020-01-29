3114f6151b4f44a59d94fc9c5d3dd9fa38ad6f97
Anthony Romesh Abeyawardena <romesh.a@outlook.com>
2020-01-29 23:41:29 +0000

    Added Balance property to budget details page

---
+++ BudgetPlanner.Services/PostProcessors/RetrieveBudgetPlanner.cs
@@ -0,0 +1,20 @@
+﻿using BudgetPlanner.Domains.Requests;
+using BudgetPlanner.Domains.Responses;
+using MediatR.Pipeline;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BudgetPlanner.Services.PostProcessors
+{
+    public class RetrieveBudgetPlanner : IRequestPostProcessor<RetrieveBudgetPlannerRequest, RetrieveBudgetPlannerResponse>
+    {
+        public Task Process(RetrieveBudgetPlannerRequest request, RetrieveBudgetPlannerResponse response, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
