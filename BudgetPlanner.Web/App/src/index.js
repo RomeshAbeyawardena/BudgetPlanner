34fcf23c5e84503e69a93aff4ec9a949945478c8
Anthony Romesh Abeyawardena <romesh.a@outlook.com>
2020-01-29 19:07:06 +0000

    Configured Babel

--- BudgetPlanner.Web/App/src/index.js
+++ BudgetPlanner.Web/App/src/index.js
@@ -1,3 +1,11 @@
-﻿import "bootstrap";
-
+﻿import 'babel-polyfill';
+import $ from "jquery";
+import "bootstrap";
+import popup from "./popup";
 require("./scss/index.scss");
+
+$(() => {
+    var popup1 = new popup();
+    popup1.init();
+    
+});
---
+++ BudgetPlanner.Web/App/src/popup/index.js
@@ -0,0 +1,11 @@
+﻿import $ from 'jquery';
+
+export default function () {
+    this.elements = [];
+    this.init = function() {
+        this.elements = $("a[popup]");
+        for (var element of this.elements) {
+            console.log($(element).attr("popup"));
+        }
+    };
+}
