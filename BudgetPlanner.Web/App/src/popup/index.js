e44232489381e1579c97e9035d0711620126804a
Anthony Romesh Abeyawardena <romesh.a@outlook.com>
2020-01-29 19:26:06 +0000

    Implement popup library

--- BudgetPlanner.Web/App/src/popup/index.js
+++ BudgetPlanner.Web/App/src/popup/index.js
@@ -1,11 +1,34 @@
 ﻿import $ from 'jquery';
 
-export default function () {
+export default function (dymamicPanelSelector) {
+    this.dymamicPanelSelector = dymamicPanelSelector;
     this.elements = [];
+    this.modes = {};
     this.init = function() {
         this.elements = $("a[popup]");
         for (var element of this.elements) {
-            console.log($(element).attr("popup"));
+            setupElement(element);
         }
     };
+    this.configureMode = function (mode, templateSelector, contentPlaceholder) {
+        if (this.modes[mode]) 
+            throw 'Mode ' + mode + ' already exists';
+
+        this.modes[mode] = { templateSelector: templateSelector, contentPlaceholder: contentPlaceholder };
+    };
+    function setupElement(element) {
+        const $element = $(element);
+        const mode = $element.attr("popup");
+        $element.on("click", () => {
+            var modeData = this.modes[mode];
+            if(!modeData)
+                throw 'Mode' + mode + ' not found';
+
+            const $dynamicPanel = $(this.dymamicPanelSelector);
+            const $template = $(modeData.templateSelector);
+
+            $dynamicPanel.html($template.html());
+        });
+        
+    }
 }
