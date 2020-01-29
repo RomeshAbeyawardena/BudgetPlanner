4c7592d0a200f8c19c997de73f60a1efa3576784
Anthony Romesh Abeyawardena <romesh.a@outlook.com>
2020-01-29 21:22:41 +0000

    Added utility js

--- BudgetPlanner.Web/App/src/popup/index.js
+++ BudgetPlanner.Web/App/src/popup/index.js
@@ -1,22 +1,29 @@
 ﻿import $ from 'jquery';
+import promise from 'promise';
 
 export default function (dymamicPanelSelector, modesHiddenFieldSelector) {
     this.dymamicPanelSelector = dymamicPanelSelector;
     this.modesHiddenFieldSelector = modesHiddenFieldSelector;
     this.elements = [];
     this.modes = {};
-    this.init = function() {
+    this.init = function () {
         this.elements = $("a[popup]");
         for (var element of this.elements) {
             setupElement(this, element);
         }
     };
     this.configureMode = function (mode, templateSelector, contentPlaceholder) {
-        if (this.modes[mode]) 
+        if (this.modes[mode])
             throw 'Mode ' + mode + ' already exists';
 
         this.modes[mode] = { templateSelector: templateSelector, contentPlaceholder: contentPlaceholder };
         return this;
+    };
+    this.getMode = function (mode) {
+        var modeData = context.modes[mode];
+        if (!modeData)
+            throw 'Mode' + mode + ' not found';
+        return modeData;
     };
     function setupElement(context, element) {
         const $element = $(element);
@@ -25,20 +32,15 @@ export default function (dymamicPanelSelector, modesHiddenFieldSelector) {
 
         $element.attr("href", "#");
         $element.attr("data-href", href);
-        
-        $element.on("click", (e) => {
-            
-            var modeData = context.modes[mode];
-            if(!modeData)
-                throw 'Mode' + mode + ' not found';
 
+        $element.on("click", (e) => {
+            const modeData = context.getMode(mode);
             const $dynamicPanel = $(context.dymamicPanelSelector);
             const $template = $(modeData.templateSelector);
+
+            $template.find(modeData.contentPlaceholder).html();
             $dynamicPanel.html($template.html());
-            console.log(e);
-            e.preventDefault();
-            return false;
         });
-        
+
     }
 }
