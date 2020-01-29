86fdd8db48e5cee84c692aa05d8f5524e378ee55
Anthony Romesh Abeyawardena <romesh.a@outlook.com>
2020-01-29 23:23:25 +0000

    Fixed formatting

--- BudgetPlanner.Domains/Constants/FormatConstants.cs
+++ BudgetPlanner.Domains/Constants/FormatConstants.cs
@@ -8,7 +8,17 @@ namespace BudgetPlanner.Domains.Constants
 {
     public static class FormatConstants
     {
+        public const string CurrencyFormat = "C2";
         public const string ShortDateFormat = "dd MMM yyyy";
         public const string LongDateFormat = "dd MMMM yyyy";
+        public static string SetFormat(string format, int index = 0)
+        {
+            var formatBuilder = new StringBuilder("{");
+            formatBuilder.Append(index);
+            formatBuilder.Append(":");
+            formatBuilder.Append(format);
+            formatBuilder.Append("}");
+            return formatBuilder.ToString();
+        }
     }
 }
