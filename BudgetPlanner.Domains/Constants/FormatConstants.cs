6be53a6490691aae0f70d7d96fa7edb79aa9d5cc
Anthony Romesh Abeyawardena <romesh.a@outlook.com>
2020-01-29 23:13:02 +0000

    Added FormatConstants

---
+++ BudgetPlanner.Domains/Constants/FormatConstants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BudgetPlanner.Domains.Constants
+{
+    public static class FormatConstants
+    {
+        public const string ShortDateFormat = "dd MMM yyyy";
+        public const string LongDateFormat = "dd MMMM yyyy";
+    }
+}
