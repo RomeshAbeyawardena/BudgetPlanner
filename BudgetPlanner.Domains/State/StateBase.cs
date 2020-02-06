430f75a7c1806456b87aba88fb31c69b4ea7928f
Anthony Romesh Abeyawardena <romesh.a@outlook.com>
2020-02-06 20:52:06 +0000

    Added blazor app

---
+++ BudgetPlanner.Domains/State/StateBase.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BudgetPlanner.Domains.State
+{
+    public class StateBase
+    {
+        public bool Visible { get; set; }
+        public bool Enabled { get; set; }
+    }
+}
