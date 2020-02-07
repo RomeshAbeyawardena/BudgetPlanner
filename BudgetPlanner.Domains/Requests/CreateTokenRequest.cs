94454508c91262e5ccc3640e48dfe8b0f928593f
Anthony Romesh Abeyawardena <romesh.a@outlook.com>
2020-02-07 22:30:13 +0000

    Added RequestToken models

---
+++ BudgetPlanner.Domains/Requests/CreateTokenRequest.cs
@@ -0,0 +1,15 @@
+﻿using BudgetPlanner.Domains.Responses;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BudgetPlanner.Domains.Requests
+{
+    public class CreateTokenRequest : IRequest<CreateTokenResponse>
+    {
+        
+    }
+}
---
+++ BudgetPlanner.Services/RequestHandlers/CreateToken.cs
@@ -0,0 +1,28 @@
+﻿using BudgetPlanner.Contracts.Services;
+using BudgetPlanner.Domains.Requests;
+using BudgetPlanner.Domains.Responses;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BudgetPlanner.Services.RequestHandlers
+{
+    public class CreateToken : IRequestHandler<CreateTokenRequest, CreateTokenResponse>
+    {
+        private readonly IRequestTokenService _requestTokenService;
+
+        public Task<CreateTokenResponse> Handle(CreateTokenRequest request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public CreateToken(IRandomStringGenerator randomStringGenerator, IRequestTokenService requestTokenService)
+        {
+            _requestTokenService = requestTokenService;
+        }
+    }
+}
