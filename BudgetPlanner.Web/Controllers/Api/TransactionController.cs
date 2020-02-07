18babd94f936915832049210d552203e45ae8901
Anthony Abeyawardena <Anthony.Abeyawardena@connellsgroup.co.uk>
2020-02-07 14:14:32 +0000

    Updated version

--- BudgetPlanner.Web/Controllers/Api/TransactionController.cs
+++ BudgetPlanner.Web/Controllers/Api/TransactionController.cs
@@ -4,11 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using DNI.Shared.Services.Abstraction;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BudgetPlanner.Web.Controllers.Api
 {
     public class TransactionController : DefaultApiController
     {
-        
+        public async Task<ActionResult> GetTransactions([Bind(Prefix = "payload")]string token)
+        {
+            var claims = GetTokenClaims(token);
+            return Ok();
+        }
     }
 }
