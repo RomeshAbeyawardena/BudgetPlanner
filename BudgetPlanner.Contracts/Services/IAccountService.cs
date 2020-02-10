24ea3c3fe9eded8a53ce7f6bc445b040089c0f90
Anthony Romesh Abeyawardena <romesh.a@outlook.com>
2020-02-10 18:54:18 +0000

    Added account store

--- BudgetPlanner.Contracts/Services/IAccountService.cs
+++ BudgetPlanner.Contracts/Services/IAccountService.cs
@@ -4,13 +4,14 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace BudgetPlanner.Contracts.Services
 {
     public interface IAccountService
     {
-        Task<Account> SaveAccount(Account account);
+        Task<Account> SaveAccount(Account account, CancellationToken cancellationToken = default);
         Task<Account> GetAccount(IEnumerable<byte> encryptedEmailAddress);
         Task<Account> GetAccount(int accountId, EntityUsage findUsage);
     }
---
+++ BudgetPlanner.Services/Stores/AccountStore.cs
@@ -0,0 +1,175 @@
+﻿using BudgetPlanner.Contracts.Services;
+using BudgetPlanner.Domains.Dto;
+using DNI.Shared.Contracts.Providers;
+using DNI.Shared.Shared.Extensions;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BudgetPlanner.Services.Stores
+{
+    public static class AccountStoreIdentityErrors
+    {
+        public static IdentityError DuplicateAccount = new IdentityError { Code = "DuplicateAccount", Description = "Duplicate account exists" };
+        public static IdentityError AccountNotFound = new IdentityError { Code = "AccountNotFound", Description = "Unable to find account" };
+    }
+
+    public class AccountStore : 
+        IUserStore<Account>, 
+        IUserPasswordStore<Account>
+    {
+        private readonly IEncryptionProvider _encryptionHelper;
+        private readonly IAccountService _accountService;
+
+        private async Task<Domains.Data.Account> GetAccount(string userId)
+        {
+            var account = new Account { EmailAddress = userId };
+            var encryptedAccount = await _encryptionHelper.Encrypt<Account, Domains.Data.Account>(account);
+            var foundAccount = await _accountService.GetAccount(encryptedAccount.EmailAddress);
+            return foundAccount;
+        }
+
+        public async Task<IdentityResult> CreateAsync(Account user, CancellationToken cancellationToken)
+        {
+            if(await FindByIdAsync(user.EmailAddress, cancellationToken) != null)
+                return IdentityResult.Failed(AccountStoreIdentityErrors.DuplicateAccount);
+
+            var encryptedAccount = await _encryptionHelper.Encrypt<Account, Domains.Data.Account>(user);
+            await _accountService.SaveAccount(encryptedAccount, cancellationToken);
+            return IdentityResult.Success;
+        }
+
+        public async Task<IdentityResult> DeleteAsync(Account user, CancellationToken cancellationToken)
+        {
+            var foundUser = await GetAccount(user.EmailAddress);
+            if(foundUser == null)
+                return IdentityResult.Failed(AccountStoreIdentityErrors.AccountNotFound);
+
+            foundUser.Active = false;
+            await _accountService.SaveAccount(foundUser);
+            return IdentityResult.Success;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+
+        protected virtual void Dispose(bool gc)
+        {
+
+        }
+
+        public async Task<Account> FindByIdAsync(string userId, CancellationToken cancellationToken)
+        {
+            var foundAccount = await GetAccount(userId);
+            if(foundAccount == null)
+                return default;
+
+            return await _encryptionHelper.Decrypt<Domains.Data.Account, Account>(foundAccount);
+        }
+
+        public Task<Account> FindByNameAsync(string normalizedUserName, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<string> GetNormalizedUserNameAsync(Account user, CancellationToken cancellationToken)
+        {
+            return await GetUserNameAsync(user, cancellationToken);
+        }
+
+        public async Task<string> GetUserIdAsync(Account user, CancellationToken cancellationToken)
+        {
+            return await Task.FromResult(user.EmailAddress);
+        }
+
+        public async Task<string> GetUserNameAsync(Account user, CancellationToken cancellationToken)
+        {
+            var account = await GetAccount(user.EmailAddress);
+
+            if(account == null)
+                return null;
+
+            var decryptedAccount = await _encryptionHelper.Decrypt<Domains.Data.Account, Account>(account);
+
+            return $"{decryptedAccount.FirstName} {decryptedAccount.LastName}";
+        }
+
+        public async Task SetNormalizedUserNameAsync(Account user, string normalizedName, CancellationToken cancellationToken)
+        {
+            var splitName = normalizedName.Split(' ');
+
+            if(splitName.Length != 2)
+                return;
+
+            var foundAccount = await GetAccount(user.EmailAddress);
+
+            if(foundAccount == null)
+                return;
+
+            user.FirstName = splitName[0];
+            user.LastName = splitName[1];
+
+            var encryptedAccount = await _encryptionHelper.Encrypt<Account, Domains.Data.Account>(user);
+
+            foundAccount.FirstName = encryptedAccount.FirstName;
+            foundAccount.LastName = encryptedAccount.LastName;
+
+            await _accountService.SaveAccount(foundAccount, cancellationToken);
+        }
+
+        public Task SetUserNameAsync(Account user, string userName, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IdentityResult> UpdateAsync(Account user, CancellationToken cancellationToken)
+        {
+            var foundAccount = await GetAccount(user.EmailAddress);
+                
+            if(foundAccount == null)
+                return IdentityResult.Failed(AccountStoreIdentityErrors.AccountNotFound);
+
+            var encryptedAccount = await _encryptionHelper.Encrypt<Account, Domains.Data.Account>(user);
+
+            encryptedAccount.Id = foundAccount.Id;
+
+            await _accountService.SaveAccount(encryptedAccount, cancellationToken);
+            return IdentityResult.Success;
+        }
+
+        public async Task SetPasswordHashAsync(Account user, string passwordHash, CancellationToken cancellationToken)
+        {
+            user.Password = passwordHash
+                .ToBase64String(Encoding.UTF8)
+                .GetBytes(Encoding.UTF8);
+
+            var account = await GetAccount(user.EmailAddress);
+            var encryptedAccount = await _encryptionHelper.Encrypt<Account, Domains.Data.Account>(user);
+            account.Password = encryptedAccount.Password;
+            await _accountService.SaveAccount(account);
+        }
+
+        public async Task<string> GetPasswordHashAsync(Account user, CancellationToken cancellationToken)
+        {
+            var encryptedAccount = await _encryptionHelper.Encrypt<Account, Domains.Data.Account>(user);
+            return Convert.ToBase64String(encryptedAccount.Password);
+        }
+
+        public async Task<bool> HasPasswordAsync(Account user, CancellationToken cancellationToken)
+        {
+            return await Task.FromResult(true);
+        }
+
+        public AccountStore(IEncryptionProvider encryptionHelper, IAccountService accountService)
+        {
+            _encryptionHelper = encryptionHelper;
+            _accountService = accountService;
+        }
+    }
+}
