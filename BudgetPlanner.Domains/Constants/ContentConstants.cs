f29bd35b13450a6ae07b6e568c5ae61642460b16
Anthony Romesh Abeyawardena <romesh.a@outlook.com>
2020-02-15 16:02:48 +0000

    Updated account editor

--- BudgetPlanner.Domains/Constants/ContentConstants.cs
+++ BudgetPlanner.Domains/Constants/ContentConstants.cs
@@ -9,6 +9,11 @@ namespace BudgetPlanner.Domains.Constants
     public static class ContentConstants
     {
         public const string RegisterContentPath = "pages/account/register";
+        public const string ConfirmPasswordLabel = "confirmPasswordLabel";
+        public const string FirstNameLabel = "firstNameLabel";
+        public const string LastNameLabel = "lastNameLabel";
+        public const string AcceptTermsOfUseLabel = "acceptTermsOfUseLabel";
+
         public const string LoginContentPath = "pages/account/login";
         public const string EmailAddressLabel = "emailAddressLabel";
         public const string PasswordLabel = "passwordLabel";
