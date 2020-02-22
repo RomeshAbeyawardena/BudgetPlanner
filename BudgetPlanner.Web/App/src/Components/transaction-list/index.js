3b65e518b0689fdc8ad42aa2c284f73e62f2ef1f
Anthony Romesh Abeyawardena <romesh.a@outlook.com>
2020-02-22 23:53:11 +0000

    Added transactionlist

--- BudgetPlanner.Web/App/src/Components/transaction-list/index.js
+++ BudgetPlanner.Web/App/src/Components/transaction-list/index.js
@@ -1,1 +1,40 @@
-﻿
+﻿import Axios from "axios";
+const template = require("./index.html");
+
+const defaultComponent = {
+    template: template,
+    props: {
+        requestUrl: String,
+        reference: String,
+        fromDate: Date,
+        toDate: Date
+    },
+    data() {
+        return {
+            from: this.fromDate,
+            to: this.toDate,
+            items: []
+        };
+    },
+    watch: {
+        fromDate(newValue) {
+            this.from = newValue;
+            getTransactions();
+        },
+        toDate(newValue) {
+            this.to = newValue;
+            getTransactions();
+        }
+    },
+    methods: {
+        getTransactions() {
+            constcontext = this;
+            return Axios.get(this.requestUrl, { params: { reference: this.reference, fromDate: this.from, toDate: this.to }}).then((e) => context,items);
+        }
+    },
+    created() {
+        this.getTransactions();
+    }
+};
+
+export default defaultComponent;
