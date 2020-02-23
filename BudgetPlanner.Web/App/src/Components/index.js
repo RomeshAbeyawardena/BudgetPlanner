8ae1079a5ba6ffebf190b9d8abca0aa960a4a423
Anthony Romesh Abeyawardena <romesh.a@outlook.com>
2020-02-23 00:09:42 +0000

    Registered Transaction list

--- BudgetPlanner.Web/App/src/Components/index.js
+++ BudgetPlanner.Web/App/src/Components/index.js
@@ -1,7 +1,9 @@
 ﻿import Constants from "./constants";
 import DashboardComponent from "./dashboard";
-
+import TransactionList from "./transaction-list";
 const components = { };
+
 components[Constants.DashboardComponent] = DashboardComponent;
+components[Constants.TransactionList] = TransactionList;
 
 export default components;
